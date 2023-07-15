17dcb369392f69411269e9bdfb714adf3e3e6723
Dx4 <139307763+DX4GREY@users.noreply.github.com>
2023-07-16 06:29:48 +0700

    Update termux.sh

--- bin/termux.sh
+++ bin/termux.sh
@@ -1,1 +1,16 @@
-
+uninstall(){
+    echo "Uninstalling FaAng..."
+    rm -rf $HOME/.faang
+    rm -rf $PREFIX/bin/faang
+    echo "FaAng uninstall task done"
+}
+case $# in
+    1)
+        if [ $1 == "-uninstall" ]; then
+            uninstall
+        fi
+        ;;
+    *)
+        python $HOME/.faang
+        ;;
+esac
