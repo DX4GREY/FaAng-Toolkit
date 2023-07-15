f16445d6b7f08faa64b65d20930c6d600eaaabdd
Dx4 <139307763+DX4GREY@users.noreply.github.com>
2023-07-16 06:29:15 +0700

    Update linux.sh

--- bin/linux.sh
+++ bin/linux.sh
@@ -1,1 +1,16 @@
-
+uninstall(){
+    echo "Uninstalling FaAng..."
+    sudo rm -rf "$HOME/.local/bin/faang"
+    sudo rm -rf "$HOME/.faang/"
+    echo "FaAng uninstall task done"
+}
+case $# in
+    1)
+        if [ $1 == "-uninstall" ]; then
+            uninstall
+        fi
+        ;;
+    *)
+        python $HOME/.faang
+        ;;
+esac
