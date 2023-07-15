0d9942601af7f37bc96aed0301f2564d548ff74f
Dx4 <139307763+DX4GREY@users.noreply.github.com>
2023-07-15 18:26:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,34 @@
+install_termux() {
+    pkg update
+    pkg upgrade
+    pkg install git
+    git clone https://github.com/DX4GREY/FaAng-Toolkit
+    cd FaAng-Toolkit
+    sh setup.sh
+    cd ../
+}
+install_linux() {
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install git
+    git clone https://github.com/DX4GREY/FaAng-Toolkit
+    cd FaAng-Toolkit
+    sh setup.sh
+    cd ../
+}
 
+detect_platform() {
+    platform=$(uname -o)
+    case $platform in
+        "Android")
+            install_termux
+            ;;
+        "GNU/Linux")
+            install_linux
+            ;;
+        *)
+            echo "Unsupported platform: $platform"
+            ;;
+    esac
+}
+detect_platform
